81ed1ed2e796b65c3bc05aec7306d36ffbcdc282
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-11-04 15:31:24 -0300

    Excluindo comentários

--- src/main/java/com/hmarcone/logistica/api/controller/ClienteController.java
+++ src/main/java/com/hmarcone/logistica/api/controller/ClienteController.java
@@ -1,14 +1,9 @@
 package com.hmarcone.logistica.api.controller;
 
-//import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.hmarcone.domain.service.CatalogoClienteService;
+
 import com.hmarcone.logistica.domain.model.Cliente;
 import com.hmarcone.logistica.domain.repository.ClienteRepository;
 import com.hmarcone.logistica.domain.service.CatalogoClienteService;
@@ -36,14 +31,10 @@ public class ClienteController {
 	private ClienteRepository clienteRepository;
 	private CatalogoClienteService catalogoClienteService;	
 		
-	
-//	@PersistenceContext
-//	private EntityManager manager;
-	
+		
 	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
-//		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 	@GetMapping("/{clienteId}")
